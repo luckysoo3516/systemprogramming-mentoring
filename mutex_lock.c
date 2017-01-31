07084d85d066283d4c99ab7460b515dc1bd559e7
luckysoo3516 <sumin328030@gmail.com>
2017-01-31 12:06:56 +0900

    this is not locked

---
+++ mutex_lock.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <pthread.h>
+
+int counter = 0;
+#define NUM_THREAD 10000
+void * thr_main(void *arg)
+{
+	counter++;
+	sleep(1);
+	return((void*)1);
+}
+
+int main(void)
+{
+	int err, i;
+	pthread_t tid[NUM_THREAD];
+	void *tret;
+	
+	for(i=0; i<NUM_THREAD; i++)
+	{
+		err = pthread_create(&tid[i], NULL, thr_main, (void*)((intptr_t)i));
+		if(err !=0) exit(1);
+	}
+	for (i=0; i<NUM_THREAD ; i++)
+	{
+		err = pthread_join(tid[i], &tret);
+		if (err != 0) exit(2);
+	}
+	printf("done %d!\n", counter);
+	return 0;
+}
